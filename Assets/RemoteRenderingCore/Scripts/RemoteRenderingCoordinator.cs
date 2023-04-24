b4cc86e66093880b43683448205c83c399031da3
seanNZH <122636264+seanNZH@users.noreply.github.com>
2023-04-24 08:28:39 -0500

    Add files via upload
    initial commit

---
+++ Assets/RemoteRenderingCore/Scripts/RemoteRenderingCoordinator.cs
@@ -0,0 +1,495 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using Microsoft.Azure.RemoteRendering;
+using Microsoft.Azure.RemoteRendering.Unity;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Events;
+
+#if UNITY_WSA
+using UnityEngine.XR.WSA;
+#endif
+
+/// <summary>
+/// Remote Rendering Coordinator is the controller for all Remote Rendering operations.
+/// </summary>
+
+// Require the GameObject with a RemoteRenderingCoordinator to also have the ARRServiceUnity component
+[RequireComponent(typeof(ARRServiceUnity))]
+public class RemoteRenderingCoordinator : MonoBehaviour
+{
+    public enum RemoteRenderingState
+    {
+        NotSet,
+        NotInitialized,
+        NotAuthorized,
+        NoSession,
+        ConnectingToExistingRemoteSession,
+        ConnectingToNewRemoteSession,
+        RemoteSessionReady,
+        ConnectingToRuntime,
+        RuntimeConnected
+    }
+
+    public static RemoteRenderingCoordinator instance;
+
+    // Account
+    // RemoteRenderingDomain must be '<region>.mixedreality.azure.com' - if no '<region>' is specified, connections will fail
+    // For most people '<region>' is either 'westus2' or 'westeurope'
+    [SerializeField]
+    private string remoteRenderingDomain = "westus2.mixedreality.azure.com";
+    public string RemoteRenderingDomain
+    {
+        get => remoteRenderingDomain.Trim();
+        set => remoteRenderingDomain = value;
+    }
+
+    [Header("Development Account Credentials")]
+    [SerializeField]
+    private string accountId = "<enter your account id here>";
+    public string AccountId {
+        get => accountId.Trim();
+        set => accountId = value;
+    }
+
+    [SerializeField]
+    private string accountDomain = "<enter your account domain here>";
+    public string AccountDomain
+    {
+        get => accountDomain.Trim();
+        set => accountDomain = value;
+    }    
+
+    [SerializeField]
+    private string accountKey = "<enter your account key here>";
+    public string AccountKey {
+        get => accountKey.Trim();
+        set => accountKey = value;
+    }
+
+    // These settings are important. All three should be set as low as possible, while maintaining a good user experience
+    // See the documentation around session management and the technical differences in session VM size
+    [Header("New Session Defaults")]
+
+    public RenderingSessionVmSize renderingSessionVmSize = RenderingSessionVmSize.Standard;
+    public uint maxLeaseHours = 0;
+    public uint maxLeaseMinutes = 20;
+
+    [Header("Other Configuration")]
+
+    [Tooltip("If you have a known active SessionID, you can fill it in here before connecting")]
+    [SerializeField]
+    private string sessionIDOverride;
+    public string SessionIDOverride {
+        get => sessionIDOverride.Trim();
+        set => sessionIDOverride = value;
+    }
+
+    // When Automatic Mode is true, the coordinator will attempt to automatically proceed through the process of connecting and loading a model
+    public bool automaticMode = true;
+
+    public event Action RequestingAuthorization;
+    public UnityEvent OnRequestingAuthorization = new UnityEvent();
+
+    public event Action AuthorizedChanged;
+    public UnityEvent OnAuthorizationChanged = new UnityEvent();
+    private bool authorized;
+    public bool Authorized
+    {
+        get => authorized;
+        set
+        {
+            if (value == true) //This is a one-way value, once we're authorized it lasts until the app is shutdown.
+            {
+                authorized = value;
+                AuthorizedChanged?.Invoke();
+            }
+        }
+    }
+
+    public delegate Task<SessionConfiguration> AccountInfoGetter();
+
+    public static AccountInfoGetter ARRCredentialGetter
+    {
+        private get;
+        set;
+    }
+
+    private RemoteRenderingState currentCoordinatorState = RemoteRenderingState.NotSet;
+    public RemoteRenderingState CurrentCoordinatorState
+    {
+        get => currentCoordinatorState;
+        private set
+        {
+            if (currentCoordinatorState != value)
+            {
+                currentCoordinatorState = value;
+                Debug.LogFormat(LogType.Log, LogOption.NoStacktrace, null, "{0}", $"State changed to: {currentCoordinatorState}");
+                CoordinatorStateChange?.Invoke(currentCoordinatorState);
+            }
+        }
+    }
+
+    public static event Action<RemoteRenderingState> CoordinatorStateChange;
+
+    public static RenderingSession CurrentSession => instance?.ARRSessionService?.CurrentActiveSession;
+
+    private ARRServiceUnity arrSessionService;
+
+    private ARRServiceUnity ARRSessionService
+    {
+        get
+        {
+            if (arrSessionService == null)
+                arrSessionService = GetComponent<ARRServiceUnity>();
+            return arrSessionService;
+        }
+    }
+
+    private async Task<SessionConfiguration> GetDevelopmentCredentials()
+    {
+        Debug.LogWarning("Using development credentials! Not recommended for production.");
+        return await Task.FromResult(new SessionConfiguration(AccountDomain, RemoteRenderingDomain, AccountId, AccountKey));
+    }
+
+    /// <summary>
+    /// Keep the last used SessionID, when launching, connect to this session if its available
+    /// </summary>
+    private string LastUsedSessionID
+    {
+        get
+        {
+            if (!string.IsNullOrEmpty(SessionIDOverride))
+                return SessionIDOverride;
+
+            if (PlayerPrefs.HasKey("LastUsedSessionID"))
+                return PlayerPrefs.GetString("LastUsedSessionID");
+            else
+                return null;
+        }
+        set
+        {
+            PlayerPrefs.SetString("LastUsedSessionID", value);
+        }
+    }
+
+    public void Awake()
+    {
+        //Forward events to Unity events
+        RequestingAuthorization += () => OnRequestingAuthorization?.Invoke();
+        AuthorizedChanged += () => OnAuthorizationChanged?.Invoke();
+
+        //Attach to event
+        AuthorizedChanged += RemoteRenderingCoordinator_AuthorizedChanged;
+
+        if (instance == null)
+            instance = this;
+        else
+            Destroy(this);
+
+        CoordinatorStateChange += AutomaticMode;
+
+        CurrentCoordinatorState = RemoteRenderingState.NotInitialized;
+    }
+
+    private void RemoteRenderingCoordinator_AuthorizedChanged()
+    {
+        if (CurrentCoordinatorState != RemoteRenderingState.NotAuthorized)
+            return; //This isn't valid from any other state than NotAuthorized
+
+
+        //We just became authorized to connect to Azure
+        InitializeSessionService();
+    }
+
+    /// <summary>
+    /// Automatic mode attempts to automatically progress through the connection and loading steps. Doesn't handle error states.
+    /// </summary>
+    /// <param name="currentState">The current state</param>
+    private async void AutomaticMode(RemoteRenderingState currentState)
+    {
+        if (!automaticMode)
+            return;
+
+        //Add a small delay for visual effect
+        await Task.Delay(1500);
+        switch (currentState)
+        {
+            case RemoteRenderingState.NotInitialized:
+                InitializeARR();
+                break;
+            case RemoteRenderingState.NotAuthorized:
+                RequestAuthorization();
+                break;
+            case RemoteRenderingState.NoSession:
+                JoinRemoteSession();
+                break;
+            case RemoteRenderingState.RemoteSessionReady:
+                ConnectRuntimeToRemoteSession();
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Initializes ARR, associating the main camera
+    /// Note: This must be called on the main Unity thread
+    /// </summary>
+    public void InitializeARR()
+    {
+        //Implement me
+        RemoteManagerUnity.InitializeManager(new RemoteUnityClientInit(Camera.main));
+
+        CurrentCoordinatorState = RemoteRenderingState.NotAuthorized;
+    }
+
+    /// <summary>
+    /// Create a new remote session manager
+    /// If the ARRCredentialGetter is set, use it as it, otherwise use the development credentials 
+    /// </summary>
+    public async void InitializeSessionService()
+    {
+        //Implement me
+        if (ARRCredentialGetter == null)
+       ARRCredentialGetter = GetDevelopmentCredentials;
+
+       var sessionConfiguration = await ARRCredentialGetter.Invoke();
+
+       ARRSessionService.OnSessionStatusChanged += OnRemoteSessionStatusChanged;
+
+       try
+   {
+       ARRSessionService.Initialize(sessionConfiguration);
+   }
+       catch (ArgumentException argumentException)
+   {
+       Debug.LogError(argumentException.Message);
+       CurrentCoordinatorState = RemoteRenderingState.NotAuthorized;
+       return;
+   }
+
+   CurrentCoordinatorState = RemoteRenderingState.NoSession;
+    }
+
+    /// <summary>
+    /// Trigger the event for checking authorization, respond to this event by prompting the user for authentication
+    /// If authorized, set Authorized = true
+    /// </summary>
+    public void RequestAuthorization()
+    {
+        RequestingAuthorization?.Invoke();
+    }
+
+    public void BypassAuthorization()
+    {
+        Authorized = true;
+    }
+
+    /// <summary>
+    /// Attempts to join an existing session or start a new session
+    /// </summary>
+    public async void JoinRemoteSession()
+    {
+        //Implement me
+        //If there's a session available that previously belonged to us, and it's ready, use it. Otherwise start a new session.
+    RenderingSessionProperties joinResult;
+    if (await IsSessionAvailable(LastUsedSessionID))
+    {
+        CurrentCoordinatorState = RemoteRenderingState.ConnectingToExistingRemoteSession;
+        joinResult = await ARRSessionService.OpenSession(LastUsedSessionID);
+    }
+    else
+    {
+        CurrentCoordinatorState = RemoteRenderingState.ConnectingToNewRemoteSession;
+        joinResult = await ARRSessionService.StartSession(new RenderingSessionCreationOptions(renderingSessionVmSize, (int)maxLeaseHours, (int)maxLeaseMinutes));
+    }
+
+    if (joinResult.Status == RenderingSessionStatus.Ready || joinResult.Status == RenderingSessionStatus.Starting)
+    {
+        LastUsedSessionID = joinResult.Id;
+    }
+    else
+    {
+        //The session should be ready or starting, if it's not, something went wrong
+        await ARRSessionService.StopSession();
+        if(LastUsedSessionID == SessionIDOverride)
+            SessionIDOverride = "";
+        CurrentCoordinatorState = RemoteRenderingState.NoSession;
+    }
+    }
+
+    public async void StopRemoteSession()
+    {
+        //Implement me
+        if (ARRSessionService.CurrentActiveSession != null)
+    {
+        await ARRSessionService.CurrentActiveSession.StopAsync();
+    }
+    }
+
+    private async Task<bool> IsSessionAvailable(string sessionID)
+    {
+        bool sessionAvailable = false;
+        try
+        {
+            RenderingSessionPropertiesArrayResult result = await ARRSessionService.Client.GetCurrentRenderingSessionsAsync();
+            if (result.ErrorCode == Result.Success)
+            {
+                RenderingSessionProperties[] properties = result.SessionProperties;
+                if (properties != null)
+                {
+                    sessionAvailable = properties.Any(x => x.Id == sessionID && (x.Status == RenderingSessionStatus.Ready || x.Status == RenderingSessionStatus.Starting));
+                }
+            }
+            else
+            {
+                Debug.LogError($"Failed to get current rendering sessions. Error: {result.Context.ErrorMessage}");
+            }
+        }
+        catch (RRException ex)
+        {
+            Debug.LogError($"Failed to get current rendering sessions. Error: {ex.Message}");
+        }
+        return sessionAvailable;
+    }
+
+    /// <summary>
+    /// Connects the local runtime to the current active session, if there's a session available
+    /// </summary>
+    public async void ConnectRuntimeToRemoteSession()
+    {
+        //Implement me
+        if (ARRSessionService == null || ARRSessionService.CurrentActiveSession == null)
+    {
+        Debug.LogError("Not ready to connect runtime");
+        return;
+    }
+
+    // Connect the local runtime to the currently connected session
+    // This session is set when connecting to a new or existing session
+
+    ARRSessionService.CurrentActiveSession.ConnectionStatusChanged += OnLocalRuntimeStatusChanged;
+    await ARRSessionService.CurrentActiveSession.ConnectAsync(new RendererInitOptions());
+    }
+
+    public void DisconnectRuntimeFromRemoteSession()
+    {
+        //Implement me
+        if (ARRSessionService == null || ARRSessionService.CurrentActiveSession == null || ARRSessionService.CurrentActiveSession.ConnectionStatus != ConnectionStatus.Connected)
+    {
+        Debug.LogError("Runtime not connected!");
+        return;
+    }
+
+    ARRSessionService.CurrentActiveSession.Disconnect();
+    ARRSessionService.CurrentActiveSession.ConnectionStatusChanged -= OnLocalRuntimeStatusChanged;
+    CurrentCoordinatorState = RemoteRenderingState.RemoteSessionReady;
+    }
+
+    /// <summary>
+    /// The session must have its runtime pump updated.
+    /// The Connection.Update() will push messages to the server, receive messages, and update the frame-buffer with the remotely rendered content.
+    /// </summary>
+    private void LateUpdate()
+    {
+        ARRSessionService?.CurrentActiveSession?.Connection?.Update();
+    }
+
+    /// <summary>
+    /// Loads a model into the remote session for rendering
+    /// </summary>
+    /// <param name="modelPath">The model's path</param>
+    /// <param name="progress">A call back method that accepts a float progress value [0->1]</param>
+    /// <param name="parent">The parent Transform for this remote entity</param>
+    /// <returns>An awaitable Remote Rendering Entity</returns>
+    public async Task<Entity> LoadModel(string modelPath, Transform parent = null, Action<float> progress = null)
+    {
+        //Implement me
+        //Create a root object to parent a loaded model to
+    var modelEntity = ARRSessionService.CurrentActiveSession.Connection.CreateEntity();
+
+    //Get the game object representation of this entity
+    var modelGameObject = modelEntity.GetOrCreateGameObject(UnityCreationMode.DoNotCreateUnityComponents);
+
+    //Ensure the entity will sync its transform with the server
+    var sync = modelGameObject.GetComponent<RemoteEntitySyncObject>();
+    sync.SyncEveryFrame = true;
+
+    //Parent the new object under the defined parent
+    if (parent != null)
+    {
+        modelGameObject.transform.SetParent(parent, false);
+        modelGameObject.name = parent.name + "_Entity";
+    }
+
+    //Load a model that will be parented to the entity
+    var loadModelParams = new LoadModelFromSasOptions(modelPath, modelEntity);
+    var loadModelAsync = ARRSessionService.CurrentActiveSession.Connection.LoadModelFromSasAsync(loadModelParams, progress);
+    var result = await loadModelAsync;
+    return modelEntity;
+        
+    }
+
+    private bool loadingTestModel = false;
+    [ContextMenu("Load Test Model")]
+    public async void LoadTestModel()
+    {
+    if(CurrentCoordinatorState != RemoteRenderingState.RuntimeConnected)
+    {
+        Debug.LogError("Please wait for the runtime to connect before loading the test model. Try again later.");
+        return;
+    }
+    if(loadingTestModel)
+    {
+        Debug.Log("Test model already loading or loaded!");
+        return;
+    }
+    loadingTestModel = true;
+
+    // Create a parent object to use for positioning
+    GameObject testParent = new GameObject("TestModelParent");
+    testParent.transform.position = new Vector3(0f, 0f, 3f);
+
+    // The 'built in engine path' is a special path that references a test model built into Azure Remote Rendering.
+    await LoadModel("builtin://Engine", testParent.transform, (progressValue) => Debug.Log($"Loading Test Model progress: {Math.Round(progressValue * 100, 2)}%"));
+    }
+
+    private async void OnRemoteSessionStatusChanged(ARRServiceUnity caller, RenderingSession session)
+    {
+        var properties = await session.GetPropertiesAsync();
+
+        switch (properties.SessionProperties.Status)
+        {
+            case RenderingSessionStatus.Error:
+            case RenderingSessionStatus.Expired:
+            case RenderingSessionStatus.Stopped:
+            case RenderingSessionStatus.Unknown:
+                CurrentCoordinatorState = RemoteRenderingState.NoSession;
+                break;
+            case RenderingSessionStatus.Starting:
+                CurrentCoordinatorState = RemoteRenderingState.ConnectingToNewRemoteSession;
+                break;
+            case RenderingSessionStatus.Ready:
+                CurrentCoordinatorState = RemoteRenderingState.RemoteSessionReady;
+                break;
+        }
+    }
+
+    private void OnLocalRuntimeStatusChanged(ConnectionStatus status, Result error)
+    {
+        switch (status)
+        {
+            case ConnectionStatus.Connected:
+                CurrentCoordinatorState = RemoteRenderingState.RuntimeConnected;
+                break;
+            case ConnectionStatus.Connecting:
+                CurrentCoordinatorState = RemoteRenderingState.ConnectingToRuntime;
+                break;
+            case ConnectionStatus.Disconnected:
+                CurrentCoordinatorState = RemoteRenderingState.RemoteSessionReady;
+                break;
+        }
+    }
+}
